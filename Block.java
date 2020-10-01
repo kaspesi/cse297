88c4d3ccbe4e2b7e62c8a9a4074a917c7a313abd
stevenshi99 <sts321@lehigh.edu>
2020-09-30 23:18:21 -0400

    data fields initiated

--- Block.java
+++ Block.java
@@ -5,50 +5,33 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;  
 import java.security.NoSuchAlgorithmException;
 import java.lang.Integer; 
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.security.NoSuchAlgorithmException;
 
 public class Block{
 
-
-    public static void main(String[] args) {
-
-        String fileNamesInput = ""; 
-        String[] fileNames = null;
-        ArrayList<String> strings = new ArrayList<>();
-        FileInputStream fis = null;
-        BufferedReader reader = null;
-        File file = null;
-        String currentDirectory = null;
-        Tree obj = new Tree();
-
-        try{
-
-            //Get user input 
-            Scanner myObj = new Scanner(System.in); 
-            System.out.println("Please the file names u dumb bing bong");
-            fileName = formatFileName(myObj.nextLine()); 
-            currentDirectory = System.getProperty("user.dir");
-            file=new File(currentDirectory + fileName);   
-            fis=new FileInputStream(file);    
-            reader = new BufferedReader(new InputStreamReader(fis));
-            
-            String line = reader.readLine();
-            while(line != null){
-                strings.add(line);
-                line = reader.readLine();
-            }   
-            Collections.sort(strings);
-            InnerNode root = obj.generateMerkleTree(strings);
-            obj.generatePatriciaEdges(root);
-            obj.printTree(root, fileName);
-
-        }
-        catch(Exception e)  
-        {  
-            e.printStackTrace();  
-        }  
-
+    private String prevHash;
+    private String rootHash;
+    private int timestamp;
+    private int target;
+    private int nonce;
+    
+    public Block (Block block, String rootHash, int target, int nonce) {
 
     }
-    
+
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        // Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    } 
 }
